0a77758d8b0797c2935971f09742e96cdbb204b0
mrSHAHZOD <128220690+mrSHAHZOD@users.noreply.github.com>
2023-06-29 19:09:27 +0500

    new

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,19 @@
+<div class="navbar-nav w-100">
+    <a href="dashboard" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+    <a href="posts" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Posts</a>
+    </div>
+</div>
+
+
+
+
+  {{--
+            fa fa-laptop me-2 comopiyute
+            far fa-file-alt me-2   page file korinishi
+            fa fa-th me-2   battin korinish
+            fa fa-chart-bar me-2  statistik
+            fa fa-table me-2  jadval
+            fa fa-keyboard me-2  --forma
+
+
+        --}}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+
+    <div class="shrink-0 flex items-center">
+        <a href="{{ route('dashboard') }}">
+            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+        </a>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ route('admin.dashboard') }}"> admin panel</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
